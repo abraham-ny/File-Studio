8e425c87afb78a65a7132a6f0926748cacad0f85
Abraham <abummoja3@gmail.com>
2024-11-06 13:40:56 +0300

    removed Top Bar title

--- src/filestudio/FXMLDocumentController.java
+++ src/filestudio/FXMLDocumentController.java
@@ -154,8 +154,6 @@ public class FXMLDocumentController implements Initializable {
     TextField dupefinderInput;
     @FXML
     ProgressBar compressorProgressBar;
-    @FXML
-    Label topBarLabel;
     String[] types = {".zip", ".tar", ".gz", ".7z", ".rar", ".tar.sz", ".tar.gz", ".tar.deflate", ".tar.xz", ".tar.bz2"};
     String archFolder = "";
     public static String pd = "https://paypal.com/donate/?hosted_button_id=A88GCN8R382B6";
@@ -591,7 +589,6 @@ public class FXMLDocumentController implements Initializable {
             organizerDirTextField.setText(activeDir);
             compressorPath.setText(activeDir);
             dupefinderInput.setText(activeDir);
-            topBarLabel.setText(activeDir + " - File Studio " + ver);
             compressorDest.setText(selectedFolder.getParent());
             JsonHandler jh = new JsonHandler();
             List<String> sel = new ArrayList<>();
