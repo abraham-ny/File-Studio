d57ce614e265e3aa442aeb80f02106eca3986668
Abraham <abummoja3@gmail.com>
2024-11-27 23:33:32 +0300

    added global interface to avoid repetitive code

--- src/filestudio/BulkRenamerController.java
+++ src/filestudio/BulkRenamerController.java
@@ -4,28 +4,56 @@
  */
 package filestudio;
 
+import static filestudio.GlobalVars.alert;
+import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
- * FXML Controller class
+ * Bulk Renamer Controller class
  *
- * @author Admin
+ * @author Abraham Moruri : abummoja3@gmail.com
  */
 public class BulkRenamerController implements Initializable {
+//browseBtn, autoWordBtn, newNameBtn
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    TextField dirPathTbx;
+    @FXML
+    TextField oldWordsTbx;
+    @FXML
+    TextField newWordTbx;
+    @FXML
+    Button browseBtn;
+    @FXML
+    Button autoWordBtn;
+    @FXML
+    Button newNameBtn;
     public static String path;
     StringBuilder sBuilder = new StringBuilder();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        dirPathTbx.setText(path);
+        autoWordBtn.setOnMouseClicked(value -> {
+            if (dirPathTbx.getText() == null) {
+                alert("Empty Path", "Enter path to folder", "Folder path is null", AlertType.ERROR);
+            }
+            if (new File(path).isDirectory()) {
+                for (File f : new File(path).listFiles()) {
+                    sBuilder.append(f.getName());
+                }
+            }
+            oldWordsTbx.setText(findMostCommonWord(sBuilder));
+        });
     }
 
     public static String findMostCommonWord(StringBuilder text) {
--- src/filestudio/GlobalVars.java
+++ src/filestudio/GlobalVars.java
@@ -4,10 +4,47 @@
  */
 package filestudio;
 
+import java.io.File;
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+
 /**
  *
  * @author Admin
  */
 public interface GlobalVars {
 
+    public static void pickDir(TextField tf, String title, String initialD) {
+        DirectoryChooser dirChooser = new DirectoryChooser();
+        if (initialD != null || !"none".equals(initialD)) {
+            dirChooser.setInitialDirectory(new File(initialD));
+        }
+        dirChooser.setTitle(title);
+        File selectedFolder = dirChooser.showDialog(tf.getScene().getWindow());
+        if (selectedFolder != null && selectedFolder.exists()) {
+            tf.setText(selectedFolder.getPath());
+        }
+    }
+
+    public static void alert(String title, String header, String message, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        ButtonType yesBtn = new ButtonType("Ok");
+        //ButtonType noBtn = new ButtonType("Close");
+        alert.getButtonTypes().setAll(yesBtn);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent()) {
+            if (result.get() == yesBtn) {
+                alert.close();
+            } else {
+                //return;
+                alert.close();
+            }
+        }
+    }
 }
