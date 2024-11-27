aa00829b3cc4ef30167f4d469279530889880eb0
Abraham <abummoja3@gmail.com>
2024-11-27 22:24:14 +0300

    minor changes

--- src/filestudio/BulkRenamerController.java
+++ src/filestudio/BulkRenamerController.java
@@ -5,6 +5,8 @@
 package filestudio;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
@@ -19,10 +21,33 @@ public class BulkRenamerController implements Initializable {
      * Initializes the controller class.
      */
     public static String path;
+    StringBuilder sBuilder = new StringBuilder();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
     }
 
+    public static String findMostCommonWord(StringBuilder text) {
+        String[] words = text.toString().split("\\W+"); // Split by non-word characters
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        // Count word frequency
+        for (String word : words) {
+            word = word.toLowerCase(); // Optional: To make it case-insensitive
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        // Find the most common word
+        String mostCommon = null;
+        int maxCount = 0;
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                mostCommon = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+
+        return mostCommon;
+    }
 }
